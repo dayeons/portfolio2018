fcf0c4a1edf6967be352b7f67863d758b28f033b
dayeons <dy7326@gmail.com>
2024-06-10 00:53:37 +0900

    0311작업수정

--- 3M/03_11/custom.js
+++ 3M/03_11/custom.js
@@ -1,63 +1,42 @@
-$(function(){
+$(function () {
+  $(" #da-thumbs .grid-item ").each(function () {
+    $(this).hoverdir();
+  });
 
-    $(function() {
-    
-        $(' #da-thumbs .grid-item ').each( function() { $(this).hoverdir(); } );
+  $(" #da-thumbs .grid-item a ").click(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("href");
+    //alert(href)
 
+    $("iframe").attr("src", href);
 
-        $(' #da-thumbs .grid-item a ').click(function(e){
-            e.preventDefault()
-            var href=$(this).attr('href')
-            //alert(href)
+    $(".iframe_display").animate({ left: "0%" }, 1000);
 
-            $('iframe').attr('src',href)
-            
-            $('.iframe_display').animate({'left':'0%'},1000)
+    $(".move").addClass("on");
+  });
+  $(".move").click(function () {
+    $(".iframe_display").animate({ left: "50%" }, 1000);
+    $(".move").removeClass("on");
+  });
+  $(".nav li").click(function () {
+    var idx = $(this).index();
+    $(".nav li").removeClass("on");
+    $(".nav li").eq(idx).addClass("on");
+  });
 
-            $('.move').addClass('on')
-          
+  var $grid = $(".grid").imagesLoaded(function () {
+    $(".grid").isotope({
+      itemSelector: ".grid-item",
 
-            
-
-        })
-        $('.move').click(function(){
-            $('.iframe_display').animate({'left':'50%'},1000)
-            $('.move').removeClass('on')
-        })
-  
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".grid-sizer",
+      },
     });
+  });
 
-
-    $('.nav li').click(function(){
-        var idx=$(this).index()
-         $('.nav li').removeClass('on')
-         $('.nav li').eq(idx).addClass('on')
-     })
- 
-     var $grid = $('.grid').imagesLoaded( function() {
-        $('.grid').isotope({
-        
-            itemSelector: '.grid-item',
- 
-            percentPosition: true,
-            masonry: {
-                columnWidth: '.grid-sizer'
-                        }
-        })
-    });
-
-
-    $('.nav li').click(function(){
-
-
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-
-
-    })
-
-
- 
-
-
+  $(".nav li").click(function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
 });
